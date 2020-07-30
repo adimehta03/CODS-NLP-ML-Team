4c198dc412f9a8d5e2141372d5a3d92507b371e4
sumukhbhat2701 <sumukhbhat2701@gmail.com>
2020-07-30 21:04:09 +0530

    state graphs on loop

--- Maps/India/components/state maps/app.js
+++ Maps/India/components/state maps/app.js
@@ -67,51 +67,121 @@ fetch("https://api.rootnet.in/covid19-in/stats/latest").then(response => respons
 
 fetch("https://api.covid19india.org/states_daily.json").then(response => response.json()).then(fdata => {
     
-    something = fdata;
-    var xLabel = [], yLabel = [], yLabelRecovered = [],yLabelDeceased = [], cumulativeDaily = [], cumulativeDailyRecovered = [], cumulativeDailyDeceased = [], statesData = ["Andaman and Nicobar Islands", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chandigarh", "Chhattisgarh", "Daman and Diu", "Delhi", "Dadra and Nagar Haveli", "Goa", "Gujarat", "Himachal Pradesh", "Harayna", "Jharkand", "Jammu and Kashmir", "Karnataka", "Kerala", "Ladakh", "Maharashtra", "Meghalaya", "Manipur", "Madhya Pradesh", "Mizoram", "Nagaland", "Orissa", "Punjab", "Puducherry", "Rajasthan", "Sikkim", "Telangana", "Tamil Nadu", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal"];
-    var checDate = [], statusCheck=[];
-    check = Object.keys(fdata.states_daily[0]);
-    for(let i=0; i<Object.keys(fdata.states_daily).length; i++){
+    // something = fdata;
+    // console.log(something);
+    // var xLabel = [], yLabel = [], yLabelRecovered = [],yLabelDeceased = [], cumulativeDaily = [], cumulativeDailyRecovered = [], cumulativeDailyDeceased = [], statesData = ["Andaman and Nicobar Islands", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chandigarh", "Chhattisgarh", "Daman and Diu", "Delhi", "Dadra and Nagar Haveli", "Goa", "Gujarat", "Himachal Pradesh", "Harayna", "Jharkand", "Jammu and Kashmir", "Karnataka", "Kerala", "Ladakh", "Maharashtra", "Meghalaya", "Manipur", "Madhya Pradesh", "Mizoram", "Nagaland", "Orissa", "Punjab", "Puducherry", "Rajasthan", "Sikkim", "Telangana", "Tamil Nadu", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal"];
+    // var checDate = [], statusCheck=[];
+    // check = Object.keys(fdata.states_daily[0]);
+    // for(let i=0; i<Object.keys(fdata.states_daily).length; i++){
         
-            if(fdata.states_daily[i].ka){
-                checDate[i] = Object.values(fdata.states_daily[i]).splice(7,1)
-                statusCheck[i] = Object.values(fdata.states_daily[i]).splice(32,1)
-                delete fdata.states_daily[i].date
-                delete fdata.states_daily[i].lk
-                // delete fdata.states_daily[i].status
-                delete fdata.states_daily[i].tt
-                delete fdata.states_daily[i].un
-                    if(fdata.states_daily[i].status == "Confirmed")
-                        yLabel.push(Number(fdata.states_daily[i].ka))
-                    if(fdata.states_daily[i].status == "Deceased")
-                        yLabelDeceased.push(Number(fdata.states_daily[i].ka))
-                    if(fdata.states_daily[i].status == "Recovered")
-                        yLabelRecovered.push(Number(fdata.states_daily[i].ka))
-            }       
-    } 
-    // console.log(yLabel);
-    xLabel = [].concat.apply([], checDate);
-    xLabel = [...new Set(xLabel)];
-    xLabel = (xLabel.filter(item => !!item))
-    // console.log(xLabel);
-    plotDaily(xLabel,yLabel,yLabelDeceased,yLabelRecovered)
+    //         if(fdata.states_daily[i].ka){
+    //             checDate[i] = Object.values(fdata.states_daily[i]).splice(7,1)
+    //             statusCheck[i] = Object.values(fdata.states_daily[i]).splice(32,1)
+    //             delete fdata.states_daily[i].date
+    //             delete fdata.states_daily[i].lk
+    //             // delete fdata.states_daily[i].status
+    //             delete fdata.states_daily[i].tt
+    //             delete fdata.states_daily[i].un
+    //                 if(fdata.states_daily[i].status == "Confirmed")
+    //                     yLabel.push(Number(fdata.states_daily[i].ka))
+    //                 if(fdata.states_daily[i].status == "Deceased")
+    //                     yLabelDeceased.push(Number(fdata.states_daily[i].ka))
+    //                 if(fdata.states_daily[i].status == "Recovered")
+    //                     yLabelRecovered.push(Number(fdata.states_daily[i].ka))
+    //         }       
+    // } 
+    // // console.log(yLabel);
+    // xLabel = [].concat.apply([], checDate);
+    // xLabel = [...new Set(xLabel)];
+    // xLabel = (xLabel.filter(item => !!item))
+    // // console.log(xLabel);
+    // plotDaily(xLabel,yLabel,yLabelDeceased,yLabelRecovered)
 
-    var cumulativeSum;
+    // var cumulativeSum;
 
-    cumulativeSum = (sum => value => sum += value)(0);
-    cumulativeDaily = yLabel.map(cumulativeSum);
-    cumulativeDaily.shift();
-    cumulativeSum = 0;
-    cumulativeSum = (sum => value => sum += value)(0);
-    cumulativeDailyDeceased = yLabelDeceased.map(cumulativeSum);
-    cumulativeDailyDeceased.shift();
-    cumulativeSum = 0;
+    // cumulativeSum = (sum => value => sum += value)(0);
+    // cumulativeDaily = yLabel.map(cumulativeSum);
+    // cumulativeDaily.shift();
+    // cumulativeSum = 0;
+    // cumulativeSum = (sum => value => sum += value)(0);
+    // cumulativeDailyDeceased = yLabelDeceased.map(cumulativeSum);
+    // cumulativeDailyDeceased.shift();
+    // cumulativeSum = 0;
 
-    cumulativeSum = (sum => value => sum += value)(0);
-    cumulativeDailyRecovered = yLabelRecovered.map(cumulativeSum);
-    cumulativeDailyRecovered.shift();
-    cumulativeSum = 0;
-    console.log(cumulativeDaily,cumulativeDailyDeceased,cumulativeDailyRecovered);
+    // cumulativeSum = (sum => value => sum += value)(0);
+    // cumulativeDailyRecovered = yLabelRecovered.map(cumulativeSum);
+    // cumulativeDailyRecovered.shift();
+    // cumulativeSum = 0;
+    // console.log(cumulativeDaily,cumulativeDailyDeceased,cumulativeDailyRecovered);
+    // for(let i=0; i<37; i++){
+    //     var divID = document.createElement('div');
+    //     divID.setAttribute('id',"tester"+i)
+    //     divID.style.width ="50%";
+    //     divID.style.height = "100%";
+    //     divID.style.display = "inline-block";
+    //     document.getElementById("testerState").appendChild(divID);
+    //     plotCumulative(xLabel,cumulativeDaily,cumulativeDailyDeceased,cumulativeDailyRecovered,divID);
+    // }
+
+    const statesData = fdata.states_daily;
+    let conf=[],rec=[],dec=[],dates=[];
+    let j=1,date=null;
+    for(let i=0;i<statesData.length;i++){
+        date = statesData[i].date;
+        delete statesData[i].date;
+        delete statesData[i].status;
+        if(j==1){
+            conf.push(statesData[i]);
+            dates.push(date);
+            j++;
+        } else if(j==2){
+            rec.push(statesData[i]);
+            j++;
+        } else {
+            dec.push(statesData[i]);
+            j = 1;
+        }
+    }
+    let recovered = {"an":[], "ap":[], "ar":[], "as":[], "br":[], "ch":[], "ct":[], "dd":[], "dl":[], "dn":[], "ga":[], "gj":[], "hp":[], "hr":[], "jh":[], "jk":[], "ka":[], "kl":[], "la":[], "ld":[], "mh":[], "ml":[], "mn":[], "mp":[], "mz":[], "nl":[], "or":[], "pb":[], "py":[], "rj":[], "sk":[], "tg":[], "tn":[], "tr":[], "tt":[], "un":[], "up":[], "ut":[], "wb":[]};
+    let deceased = {"an":[], "ap":[], "ar":[], "as":[], "br":[], "ch":[], "ct":[], "dd":[], "dl":[], "dn":[], "ga":[], "gj":[], "hp":[], "hr":[], "jh":[], "jk":[], "ka":[], "kl":[], "la":[], "ld":[], "mh":[], "ml":[], "mn":[], "mp":[], "mz":[], "nl":[], "or":[], "pb":[], "py":[], "rj":[], "sk":[], "tg":[], "tn":[], "tr":[], "tt":[], "un":[], "up":[], "ut":[], "wb":[]};
+    let confirmed = {"an":[], "ap":[], "ar":[], "as":[], "br":[], "ch":[], "ct":[], "dd":[], "dl":[], "dn":[], "ga":[], "gj":[], "hp":[], "hr":[], "jh":[], "jk":[], "ka":[], "kl":[], "la":[], "ld":[], "mh":[], "ml":[], "mn":[], "mp":[], "mz":[], "nl":[], "or":[], "pb":[], "py":[], "rj":[], "sk":[], "tg":[], "tn":[], "tr":[], "tt":[], "un":[], "up":[], "ut":[], "wb":[]};
+    let everythingR=[];
+    let everythingC=[];
+    let everythingD=[];
+    let states = Object.keys(statesData[0]);
+
+    
+    for(let i=0;i<rec.length;i++){
+        everythingR.push(Object.values(rec[i]));
+        everythingC.push(Object.values(conf[i]));
+        everythingD.push(Object.values(dec[i]));
+    }
+    
+    function cummulativeSum(a) {
+        let result = [a[0]];
+    
+        for(let i = 1; i < a.length; i++) {
+          result[i] = result[i - 1] + a[i];
+        }
+    
+        return result;
+    };
+
+    for(let i=0;i<everythingR.length;i++){
+        for(let j=0;j<everythingR[i].length;j++){
+            recovered[states[j]].push(Number(everythingR[i][j]));
+            deceased[states[j]].push(Number(everythingD[i][j]));
+            confirmed[states[j]].push(Number(everythingC[i][j]));
+        }
+    }
+
+    for(let i=0;i<Object.keys(confirmed).length;i++){
+        recovered[states[i]] = cummulativeSum(recovered[states[i]]);
+        deceased[states[i]] = cummulativeSum(deceased[states[i]]);
+        confirmed[states[i]] = cummulativeSum(confirmed[states[i]]);
+    }
+    // console.log(recovered,deceased,confirmed)   # statewise data in datewise order of dates list above
+
     for(let i=0; i<37; i++){
         var divID = document.createElement('div');
         divID.setAttribute('id',"tester"+i)
@@ -119,12 +189,12 @@ fetch("https://api.covid19india.org/states_daily.json").then(response => respons
         divID.style.height = "100%";
         divID.style.display = "inline-block";
         document.getElementById("testerState").appendChild(divID);
-        plotCumulative(xLabel,cumulativeDaily,cumulativeDailyDeceased,cumulativeDailyRecovered,divID);
+        plotCumulative(dates,confirmed[states[i]],deceased[states[i]],recovered[states[i]],divID,states[i]);
     }
-    
+
 });
 
-function plotCumulative(xLabel,cumulativeDaily,cumulativeDailyDeceased,cumulativeDailyRecovered,divID){
+function plotCumulative(xLabel,cumulativeDaily,cumulativeDailyDeceased,cumulativeDailyRecovered,divID,state){
     // console.log(xLabel);
     var trace1 = {
         x: xLabel,
@@ -148,7 +218,7 @@ function plotCumulative(xLabel,cumulativeDaily,cumulativeDailyDeceased,cumulativ
       var data = [trace1,trace3,trace2];
       
       var layout = {
-          title: "Karnataka",
+          title: state,
           xaxis: { showticklabels: false }
     };
       Plotly.newPlot(divID.id, data,layout, {displayModeBar:false});
